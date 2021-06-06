7b40384df921e39e24b202fc83b0237fc7b68d4d
Diormsmith <medinadiorfelis@gmail.com>
2021-06-05 21:22:13 -0400

    umm

---
+++ src/app/home/hazte-voluntario/hazte-voluntario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hazte-voluntario',
+  templateUrl: './hazte-voluntario.component.html',
+  styleUrls: ['./hazte-voluntario.component.scss']
+})
+export class HazteVoluntarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,22 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Material} from '../Material'
-import { MisionComponent } from './mision/mision.component';
-import { VisionComponent } from './vision/vision.component';
 import { EstructuraComponent } from './estructura/estructura.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { AboutComponent } from './about/about.component';
+import {HazteVoluntarioComponent} from './hazte-voluntario/hazte-voluntario.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AlianzasComponent } from './alianzas/alianzas.component';
+import { SedesComponent } from './sedes/sedes.component';
+import { JornadasComponent } from './jornadas/jornadas.component';
+import { JornadaComponent } from './jornada/jornada.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ImagenesComponent } from './imagenes/imagenes.component'
 
 
 
 @NgModule({
-  declarations: [MisionComponent, VisionComponent, EstructuraComponent],
+  declarations: [ EstructuraComponent, ServiciosComponent, AboutComponent,HazteVoluntarioComponent,
+    ContactoComponent, AlianzasComponent, SedesComponent, JornadasComponent, JornadaComponent,
+     ProyectosComponent, ImagenesComponent],
   imports: [
     CommonModule,
     Material
   ],
   exports: [
-    MisionComponent,
-    VisionComponent,
-    EstructuraComponent
+
+    EstructuraComponent,
+    ServiciosComponent,
+    AboutComponent,
+    HazteVoluntarioComponent,
+    ContactoComponent,
+    AlianzasComponent,
+    SedesComponent,
+    JornadasComponent,
+    JornadaComponent,
+    ProyectosComponent,
+    ImagenesComponent
   ]
 })
 export class HomeModule { }
